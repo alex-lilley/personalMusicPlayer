7f7743850ba40700c4f822dd653e3c85bc9f3411
alex lilley <alilley4@my.bcit.ca>
2018-07-21 20:22:01 +0100

    added removal of songs from playlist

--- home.py
+++ home.py
@@ -5,8 +5,8 @@ import os
 
 app = Flask(__name__)
 
-initializedData = {"song":{}, "playlist":{}}
-musicDataPath = "./musicData.json"
+initialized_data = {"song":{}, "playlist":{}}
+music_data_path = "./musicData.json"
 # Prevent flask from caching js for developement
 @app.after_request
 def add_header(r):
@@ -20,27 +20,27 @@ def add_header(r):
     r.headers['Cache-Control'] = 'public, max-age=0'
     return r
 
-def getMusicData():
-    if not os.path.exists(musicDataPath):
-            with open(musicDataPath, "w") as  jsonFile:
-                json.dump(initializedData, jsonFile, indent=4)
+def get_music_data():
+    if not os.path.exists(music_data_path):
+            with open(music_data_path, "w") as  json_file:
+                json.dump(initialized_data, json_file, indent=4)
    
-    with open(musicDataPath, "r") as jsonFile:
+    with open(music_data_path, "r") as json_file:
         try:
-            return json.load(jsonFile)
+            return json.load(json_file)
         except:
-            return initializedData      
+            return initialized_data      
 
 @app.route("/", methods=['GET'])
 def home():
     return render_template('main.html')
 
-@app.route("/appData", methods=['GET'])
+@app.route("/app_data", methods=['GET'])
 def setup():  
-    return json.dumps(getMusicData())
+    return json.dumps(get_music_data())
 
-@app.route("/recordSong", methods=['POST'])
-def recordSong():
+@app.route("/record_song", methods=['POST'])
+def record_song():
     data = json.loads(request.form['vidInfo'])
 
     url = request.form['url']
@@ -54,23 +54,23 @@ def recordSong():
         if "?" in url:
             url = url.split("?")[0]
 
-        vidID = url[-11:]
+        vid_ID = url[-11:]
 
-        songName = data["title"]+"-"+data['author_name']
-        newSong = {vidID:{"url": url, "author":data['author_name'], 'title':data['title'], "vidID": vidID, "type":"song"}}
+        song_name = data["title"]+"-"+data['author_name']
+        new_song = {vid_ID:{"url": url, "author":data['author_name'], 'title':data['title'], "vidID": vid_ID, "type":"song"}}
 
-        songList = getMusicData()
-        songList["song"].update(newSong)
-        with open(musicDataPath, "w+") as jsonFile:
-            json.dump(songList, jsonFile, indent=4)
-            print("added new song to playlist: " + songName, file=sys.stdout)
-            return json.dumps(songList["song"][vidID])
+        song_list = get_music_data()
+        song_list["song"].update(new_song)
+        with open(music_data_path, "w+") as json_file:
+            json.dump(song_list, json_file, indent=4)
+            print("added new song to playlist: " + song_name, file=sys.stdout)
+            return json.dumps(song_list["song"][vid_ID])
 
     else:
         return "url not included in request", 400
 
-@app.route("/recordPlaylist", methods=['POST'])
-def recordPlaylist():
+@app.route("/record_playlist", methods=['POST'])
+def record_playlist():
     print(request.form, file=sys.stdout)
     playlist_name = request.form.getlist('name')[0]
     songs = request.form.getlist('songs[]')
@@ -78,10 +78,50 @@ def recordPlaylist():
     if (playlist_name is not None or playlist_name != "") and songs:
         playlist = {playlist_name :{ 'songs': songs, 'type':'playlist'}}
 
-        musicData = getMusicData()
-        musicData["playlist"].update(playlist)
-        with open(musicDataPath, 'w') as jsonFile:
-            json.dump(musicData, jsonFile, indent=4)
+        music_data = get_music_data()
+        music_data["playlist"].update(playlist)
+        with open(music_data_path, 'w') as json_file:
+            json.dump(music_data, json_file, indent=4)
         return "", 200
     else:
         return "incorrect data format sent to server", 400
+
+@app.route("/delete_playlist", methods=['POST'])
+def delete_playlist():
+    music_data = get_music_data()
+    playlist_name = request.form.getlist('playlistName')[0]
+    includes_songs = request.form.getlist('includeSongs')[0]
+
+    if includes_songs == '0':
+        del music_data["playlist"][playlist_name]
+        with open(music_data_path, 'w') as json_file:
+            json.dump(music_data, json_file, indent=4)
+        print("hero")
+    else:
+        song_list = music_data["playlist"][playlist_name]['songs'].copy()
+        print(music_data["playlist"][playlist_name]['songs'])
+        for song in song_list:
+            print(song, file=sys.stdout)
+            for playlist in music_data["playlist"]:
+                if song in music_data["playlist"][playlist]['songs']:
+                    music_data["playlist"][playlist]["songs"].remove(song)
+                    print(music_data["playlist"][playlist]["songs"])
+            del music_data["song"][song]
+        del music_data["playlist"][playlist_name]
+        with open(music_data_path, 'w') as json_file:
+            json.dump(music_data, json_file, indent=4)
+    return "", 200
+
+@app.route("/delete_song", methods=["POST"])
+def delete_song():
+    music_data = get_music_data()
+    songId = request.form.getlist('vidId')[0]
+    for playlist in music_data["playlist"]:
+        print("hello")
+        if songId in music_data["playlist"][playlist]['songs']:
+            music_data["playlist"][playlist]["songs"].remove(songId)
+            print(music_data["playlist"][playlist]["songs"])
+    del music_data["song"][songId]
+    with open(music_data_path, 'w') as json_file:
+            json.dump(music_data, json_file, indent=4)
+    return "", 200
--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,41 @@
 import swal from 'sweetalert2'
 
+
+function remove(array, element) {
+    const index = array.indexOf(element);
+    
+    if (index !== -1) {
+        array.splice(index, 1);
+    }
+}
+
+var collectionData;
+
+function playNextSong() {
+    player.setPlaybackQuality("hd720");
+    singleSong = false;
+    currentSong += 1;
+    if (currentSong < songPlayList.length) {
+        player.loadVideoById(songPlayList[currentSong]);
+        player.playVideo();
+        highLightCurrentSong();
+    }
+
+    else {
+
+        if(looping && (songPlayList.length > 0)){
+            currentSong = 0;
+            player.loadVideoById(songPlayList[currentSong]);
+            player.playVideo();
+            highLightCurrentSong();
+        }
+
+        else {
+            console.log("Playlist has finished");
+        }
+    }
+}
+
 var player;
 function pollForYTLoaded() {
     // Before we can create the Player object, it needs to
@@ -8,20 +44,12 @@ function pollForYTLoaded() {
     if (YT.loaded != 1) {
         return setTimeout(pollForYTLoaded, 50);
     }
-    function playNextSong() {
-        currentSong += 1;
-        if (currentSong < songPlayList.length) {
-            player.loadVideoById(songPlayList[currentSong]);
-            player.playVideo();
-        }
-        else {
-            console.log("Playlist has finished");
-        }
-    }
 
     player = new YT.Player('vid-player', {
         width: 860,
         height: 860 * 9 / 16,
+        videoId: '',
+        playerVars:{rel:0},
         events: {
             onReady: function (e) {
                 console.log("Player is ready");
@@ -43,20 +71,70 @@ function pollForYTLoaded() {
 }
 pollForYTLoaded();
 
+$("#collection").hide();
 var songPlayList = []
 var currentSong = 0
-var collectionData;
+var singleSong = false;
+var looping = false;
 
-$("#play").on("click", function () {
+$("#loop-playlist").on("click",function(){
+    if (looping){
+        looping = false;
+        $("#loop-playlist").css("color", "#888888");
+    }
+    else {
+        looping = true;
+        $("#loop-playlist").css("color", "rgba(255, 255, 255, 0.88)");
+    }
+})
+
+function highLightCurrentSong () {
+    $(".playing").removeClass("playing");
+    let playinsSong = $("#playlist").children()[currentSong];
+    playinsSong.classList.add("playing");
+}
+
+$("#start-playlist").on("click", function () {
+    startPlaylist();
+})
+
+$("#play-all").on("click",function(){
+    var playlist = $("#playlist");
+    $.each(collectionData["song"], function(key, value){
+        let title = value["title"];
+        let author = value["author"];
+        let vidId = value["vidID"];
+
+        listSong(title, author, vidId, playlist);
+        $(`#${vidId}`).addClass("playlist-content")
+    })
+
+    startPlaylist();
+})
+
+$("#pause-song").on("click", function(){
+    if (YT.PlayerState.PLAYING) {
+        player.pauseVideo();
+    }
+})
+
+$("#play-song").on("click", function(){
+    if (YT.PlayerState.PAUSED) {
+        player.playVideo();
+    }
+})
+
+function startPlaylist() {
     currentSong = 0;
     if (songPlayList[0]) {
-        player.loadVideoById(songPlayList[0])
+        player.loadVideoById(songPlayList[0]);
+        highLightCurrentSong();
         player.playVideo();
     }
     else {
-        console.log("no songs added to playlist")
+        console.log("no songs added to playlist");
     }
-})
+}
 
 $('#record-song').on('click', function (e) {
     swal({
@@ -77,7 +155,26 @@ $('#record-song').on('click', function (e) {
 })
 
 $("#record-playlist").on("click", function () {
-    let playlistName = $("#playlist-name").val().trim();
+    swal({
+        input: 'text',
+        title: 'Add new Playlist',
+        inputPlaceholder: 'Enter the new Playlist name',
+        showCancelButton: true
+    })
+        .then(res => { 
+            if (res.dismiss) {
+                console.log("modal dismissed")
+            }
+            else {
+                recordPlaylist(res.value)
+            }
+        })
+
+
+})
+
+function recordPlaylist(playlist) {
+    let playlistName = playlist
 
     if (playlistName == null || playlistName.length === 0) {
         console.error("no name entered");
@@ -90,65 +187,161 @@ $("#record-playlist").on("click", function () {
     $.ajax({
         data: {
             songs: songPlayList,
-            name: $("#playlist-name").val()
+            name: playlistName
         },
         type: "POST",
-        url: "recordPlaylist"
+        url: "record_playlist"
     })
         .then(refreshView())
         .catch(err => console.error(err));
+}
+
+$("#previous-song").on("click", function() {
+    if(songPlayList.length >= 1){
+        if (currentSong > 0){
+            currentSong -=1;
+        }
+        player.loadVideoById(songPlayList[currentSong]);
+        highLightCurrentSong();
+        player.playVideo();
+    } 
 })
+$("#next-song").on("click", function() {
+    playNextSong();
+})
+
+function checkPlaylist(songId) {
+    if (songPlayList.includes(songId)) {
+        return true
+    }
+    else {
+        return false
+    }
+}
+
+function listSong (title, author, vidId, playlist, parent=false){
+    
+    songPlayList.push(vidId);
+
+    if(parent){
+        parent.addClass("playlist-content");
+    }
+
+    var listedSong = $(`<div class="song" />`);
+    var listedSongTitle = $(`<div class="song-title">${title}</div>`);
+    var listedSongAuthor = $(`<div class="song-author sub-text">${author}</div>`);
+    var listedSongControls = $(`<div class=playlist-song-controls />`);
+    
+    var listedSongPlay = $(`<i class="fas fa-play-circle fa-2x icon-button" title="Play this Song"></i>`);
+    listedSongPlay.on("click", function () {
+        currentSong = songPlayList.indexOf(vidId);
+        player.loadVideoById(vidId);
+        highLightCurrentSong();
+        player.playVideo();
+    })
+
+    var listedSongRemove = $(`<i class="fas fa-ban fa-2x icon-button" title="Delete this song from playlist">
+    </i>`)
+    listedSongRemove.on("click", function() {
+        if (songPlayList.indexOf(vidId) < currentSong){
+            currentSong -= 1;
+        }
+        else if (songPlayList.indexOf(vidId) === currentSong) {
+            playNextSong();
+            currentSong -= 1;
+        }
+        remove(songPlayList, vidId);
+        $(listedSongRemove).parent().parent().remove();
+    })
+
+    listedSong.append(listedSongTitle);
+    listedSong.append(listedSongAuthor);
+    listedSongControls.append(listedSongPlay);
+    listedSongControls.append(listedSongRemove);
+    listedSong.append(listedSongControls)
+    playlist.append(listedSong);
+}
 
 function refreshView() {
     return $.ajax({
         type: "GET",
         dataType: "json",
-        url: "/appData"
+        url: "/app_data"
     })
         .then(function (appData) {
             collectionData = appData;
-            var songs = $("#songs");
+            var collection = $("#collection");
             var playlist = $("#playlist");
             var playlists = $("#playlists");
-            songs.empty();
-            playlist.empty();
+            collection.empty();
+            // playlist.empty();
             playlists.empty();
 
             $.each(appData["song"], function (key, appObject) {
 
+                var title = appObject.title;
                 var author = appObject.author;
                 var vidId = appObject.vidID;
-                var title = appObject.title;
 
-                var song = $(`<div class="song" />`);
+                var song = $(`<div class="song" id="${vidId}" />`);
                 var songControls = $(`<div class="song-controls" />`);
                 var songTitle = $(`<div class="song-title">${title}</div>`);
                 var songAuthor = $(`<div class="song-author sub-text">${author}</div>`);
 
                 var songPlay = $(`<i class="fas fa-play-circle fa-2x icon-button" title="Play this Song"></i>`);
                 songPlay.on('click', function () {
+                    if (!singleSong) {
+                        currentSong -= 1;
+                        singleSong = true;
+                    }
+                    $(".playlist-content").removeClass("playlist-content");
                     player.loadVideoById(vidId);
                     player.playVideo();
+                    
                 })
 
                 var songAdd = $(`<i class="fas fa-plus-circle fa-2x icon-button" title="Add song to current playlist"></i>"`);
                 songAdd.on('click', function () {
-                    songPlayList.push(vidId);
-                    var listedSong = $(`<button class="song">${title}</button>`);
-                    listedSong.on("click", function () {
-                        currentSong = songPlayList.indexOf(vidId);
-                        player.loadVideoById(vidId);
-                        player.playVideo();
-                    })
-                    playlist.append(listedSong);
+                    if(!checkPlaylist(vidId)) {
+                        listSong(title, author, vidId, playlist, $(this).parent().parent());
+                    }        
+
                 });
 
+                var songDelete = $(`<i class="fas fa-ban fa-2x icon-button" title="Delete this song">
+                </i>`)
+
+                songDelete.on("click", function() {
+                    swal({
+                        title:song.title,
+                        text:"Are you sure you want to delete this Song",
+                        showCancelButton:true
+                    })
+                    .then(res =>  {
+                        if (res.dismiss) {
+                            console.log("modal dismissed")
+                        }
+                        else {
+                            $.ajax({
+                                data:{
+                                    vidId:vidId
+                                },
+                                type:"POST",
+                                url:"/delete_song"
+                                
+                            })
+                            .then(refreshView())
+                        }
+                    })
+                })
+
+                songControls.append(songDelete);
                 songControls.append(songPlay);
                 songControls.append(songAdd);
                 song.append(songControls);
                 song.append(songTitle);
                 song.append(songAuthor);
-                songs.append(song);
+                collection.append(song);
             });
 
             // Add playlist retrieval here
@@ -161,26 +354,56 @@ function refreshView() {
 
                 var playlistPlay = $(`<i class="fas fa-play-circle fa-2x icon-button" title="Play this playlist"></i>`);
                 playlistPlay.on("click", function () {
+                    $(this).parent().parent().addClass("playlist-content");
+
                     $.each(songsList, function (song) {
                         var songData = appData["song"][songsList[song]];
-                        var vidId = songData.vidID
-                        songPlayList.push(vidId);
-
-                        var listedSong = $(`<button class="song">${songData.title}</button>`);
-                        listedSong.on("click", function () {
-                            currentSong = songPlayList.indexOf(vidId);
-                            player.loadVideoById(vidId);
-                            player.playVideo();
-                        })
-                        playlist.append(listedSong);
+                        var title = songData["title"];
+                        var author = songData["author"];
+                        var vidId = songData.vidID;
+                        if(!checkPlaylist(vidId)) {
+                            $(`#${vidId}`).addClass("playlist-content")
+                            listSong(title, author, vidId, playlist, $(this).parent().parent());
+                        }
                     })
                 })
 
-                playlistControls.append(playlistPlay)
-                newPlaylist.append(playlistControls)
-                newPlaylist.append(playlistName)
-                newPlaylist.append(playlistSongCount)
-                playlists.append(newPlaylist)
+                var playlistDelete = $(`<i class="fas fa-ban fa-2x icon-button" title="Delete this playlist">
+                </i>`)
+
+                playlistDelete.on("click", function() {
+                    swal({
+                        title:key,
+                        text:"Are you sure you want to delete this playlist",
+                        input:"checkbox",
+                        inputPlaceholder: "Also delete songs in playlist from collection",
+                        showCancelButton:true
+                    })
+                    .then(res =>  {
+                        if (res.dismiss) {
+                            console.log("modal dismissed")
+                        }
+                        else {
+                            $.ajax({
+                                data:{
+                                    includeSongs:res.value,
+                                    playlistName:key
+                                },
+                                type:"POST",
+                                url:"/delete_playlist"
+                                
+                            })
+                            .then(refreshView())
+                        }
+                    })
+                })
+
+                playlistControls.append(playlistDelete);
+                playlistControls.append(playlistPlay);
+                newPlaylist.append(playlistControls);
+                newPlaylist.append(playlistName);
+                newPlaylist.append(playlistSongCount);
+                playlists.append(newPlaylist);
             })
 
         })
@@ -198,7 +421,7 @@ function recordSong(url) {
         .then(function (data) {
             var videoData = data
             return $.ajax({
-                url: "/recordSong",
+                url: "/record_song",
                 type: "POST",
                 data: {
                     url: url,
@@ -217,18 +440,47 @@ $("#clear-playlist").on("click", function () {
     songPlayList = [];
     currentSong = 0;
     $("#playlist").empty();
+    $(".playlist-content").removeClass("playlist-content");
 })
 
-$('#filter-songs').on("keyup", function () {
-    var filterString = $('#filter-songs').val().toLowerCase();
-    $.each($(".song"), function (index, song) {
-        var songTitle = $(song).find(".song-title").text().toLowerCase();
-        var songAuthor = $(song).find(".song-author").text().toLowerCase();
-        if (songTitle.indexOf(filterString) === -1 && songAuthor.indexOf(filterString) === -1) {
-            song.style.display = "none";
-        }
-        else {
-            song.style.display = "block";
-        }
-    })
+
+var shown = "playlists"
+
+$("#show-playlists").on("click", function(){
+    shown = "playlists";
+    $("#playlists").show();
+    $("#collection").hide();
+})
+
+$("#show-collection").on("click", function(){
+    shown = "collection";
+    $("#playlists").hide();
+    $("#collection").show();
+})
+
+$('#filter-collection').on("keyup", function () {
+    var filterString = $('#filter-collection').val().toLowerCase();
+    if (shown === "collection"){
+        $.each($(".song"), function (index, song) {
+            var songTitle = $(song).find(".song-title").text().toLowerCase();
+            var songAuthor = $(song).find(".song-author").text().toLowerCase();
+            if (songTitle.indexOf(filterString) === -1 && songAuthor.indexOf(filterString) === -1) {
+                song.style.display = "none";
+            }
+            else {
+                song.style.display = "block";
+            }
+        })
+    }
+    else if (shown === "playlists") {
+        $.each($(".playlist"), function (index, playlist) {
+            var playlistName = $(playlist).find(".playlist-name").text();
+            if (playlistName.indexOf(filterString) === -1){
+                playlist.style.display = "none";
+            }
+            else {
+                playlist.style.display = "block";
+            }
+        })
+    }
 })
